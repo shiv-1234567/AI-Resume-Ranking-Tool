51e746a12af632797633302e549a029fd9be7a02
Shivendra prasad Mishra <shivendramishra346@gmail.com>
2025-07-16 21:11:32 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,5 +1,14 @@
 import fitz  # PyMuPDF
+
 import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 nlp = spacy.load("en_core_web_sm")
 
